98c378db2ffb527aabba12795e0939e9557da453
Shivam Kumar <kumarshivam16062004@gmail.com>
2022-12-16 09:04:45 +0530

    version 1.0

---
+++ function.js
@@ -0,0 +1,75 @@
+
+
+//function declaration
+
+//function declaration
+
+// function sum(a,b){
+//     console.log(a + b)
+// }
+// sum(10,20)
+// console.log(sum(10,20))
+// function sum(a,b){
+//     return(a + b)
+// }
+
+//function expression
+//when u store a function in a variable 
+
+// let sum1 = function sum(a,b){
+//     return a + b
+// }
+// console.log(sum1(10,20))
+
+//anonymous function
+//functions without name
+//store the function in a variable or pass it into other function
+// let sum = function (a,b){ //function expresion
+//     return a + b
+// }
+// console.log(sum(10,20))
+
+// function printName(name, callback){
+
+//     console.log(name)
+//     callback()
+// }
+// printName('John', function(){
+//     console.log('anonymous function inside a function')
+// })
+
+
+// function x(){
+//     let a = 20
+
+//     return function(){
+//         console.log(a)
+//     }
+// }
+// // x()()
+// let y = x()
+// y()
+
+
+// function x(){
+//     let a = 20
+
+//     return function y(){
+//         console.log(a)
+//     }
+// }
+// // x()()
+// let z = x()
+// z()
+
+//arrow functions
+
+// let sum = (a,b) => {
+//     return a + b
+// }
+// console.log(sum(10,20))
+
+// let sum = (a,b) => a + b
+// console.log(sum(10,20))
+
+
